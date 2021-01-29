3a2a4a3a33aebd2f6e643ec2c352fd560d47344c
JAK719 <jkepic19@gmail.com>
2021-01-29 15:33:02 -0500

    Completed tests 1-5

--- CarGarage/Car.cs
+++ CarGarage/Car.cs
@@ -12,5 +12,46 @@ namespace CarGarage
         public int FuelLevel { get; set; }
         public bool AutoBrakeActive { get; set; }
 
+
+        public string SetMake(string make)
+        {
+            Make = make;
+            return make;
+        }
+
+        public string SetModel(string model)
+        {
+            Model = model;
+            return model;
+        }
+
+        public int GetSpeed()
+        {
+            return Speed;
+        }
+
+        public bool IsSpeeding()
+        {
+            if(Speed >= 71)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+      
+
+        }
+
+        public int Accelerate()
+        {
+
+            Speed = Speed + 10;
+            return Speed;
+          
+        }
+
+
     }
 }
